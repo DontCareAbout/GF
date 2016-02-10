c15c773bcaddc61624ddc158c61439984a0ef6fd
MontyPan <monty.pan@dontcareabout.us>
2016-02-10 18:06:17 +0800

    新增 ImageUtil.toResource()

---
+++ src/main/java/us/dontcareabout/gwt/client/util/ImageUtil.java
@@ -0,0 +1,40 @@
+package us.dontcareabout.gwt.client.util;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.safehtml.shared.SafeUri;
+import com.google.gwt.safehtml.shared.UriUtils;
+import com.google.gwt.user.client.ui.Image;
+
+public class ImageUtil {
+	/** 將一個 {@link Image} 轉成 {@link ImageResource} */
+	public static ImageResource toResource(final Image image) {
+		return new ImageResource() {
+			@Override
+			public int getHeight() {
+				return image.getHeight();
+			}
+
+			@Override
+			public SafeUri getSafeUri() {
+				return UriUtils.fromSafeConstant(image.getUrl());
+			}
+
+			@Deprecated
+			@Override
+			public String getURL() {
+				return image.getUrl();
+			}
+
+			@Override
+			public int getWidth() {
+				return image.getWidth();
+			}
+
+			// ==== Don't Care 區 ==== //
+			@Override public String getName() { return null; }
+			@Override public int getLeft() { return 0; }
+			@Override public int getTop() { return 0; }
+			@Override public boolean isAnimated() { return false; }
+		};
+	}
+}
