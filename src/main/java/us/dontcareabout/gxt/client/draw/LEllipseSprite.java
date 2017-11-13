86c21f2e34d6798daab2dd9ed0d3d5739c9d03e4
MontyPan <monty.pan@dontcareabout.us>
2017-11-13 11:45:32 +0800

    增加：cicle、ellipse、image、rectangle 四個 sprite 的 Layer 版實作

---
+++ src/main/java/us/dontcareabout/gxt/client/draw/LEllipseSprite.java
@@ -0,0 +1,88 @@
+package us.dontcareabout.gxt.client.draw;
+
+import com.sencha.gxt.chart.client.draw.sprite.EllipseSprite;
+
+public class LEllipseSprite extends EllipseSprite implements LSprite {
+	private Parameter parameter = new Parameter();
+	private Layer layer;
+
+	@Override
+	public void setLayer(Layer layer) {
+		this.layer = layer;
+	}
+
+	@Override
+	public void setCenterX(double value) {
+		if (parameter != null && parameter.lock) {
+			throw new UnsupportedOperationException("Use setLX() instead.");
+		}
+
+		super.setCenterX(value);
+	}
+
+	@Override
+	public void setLX(double value) {
+		parameter.x = value;
+
+		if (layer == null) { return; }
+
+		parameter.lock = false;
+		setCenterX(layer.getX() + parameter.x);
+		parameter.lock = true;
+	}
+
+	@Override
+	public double getLX() {
+		return parameter.x;
+	}
+
+	@Override
+	public void setCenterY(double value) {
+		if (parameter != null && parameter.lock) {
+			throw new UnsupportedOperationException("Use setLY() instead.");
+		}
+
+		super.setCenterY(value);
+	}
+
+	@Override
+	public void setLY(double value) {
+		parameter.y = value;
+
+		if (layer == null) { return; }
+
+		parameter.lock = false;
+		setCenterY(layer.getY() + parameter.y);
+		parameter.lock = true;
+	}
+
+	@Override
+	public double getLY() {
+		return parameter.y;
+	}
+
+	@Override
+	public void setZIndex(int zIndex) {
+		if (parameter != null && parameter.lock) {
+			throw new UnsupportedOperationException("Use setLZIndex() instead.");
+		}
+
+		super.setZIndex(zIndex);
+	}
+
+	@Override
+	public void setLZIndex(int value) {
+		parameter.zIndex = value;
+
+		if (layer == null) { return; }
+
+		parameter.lock = false;
+		setZIndex(layer.getZIndex() + parameter.zIndex);
+		parameter.lock = true;
+	}
+
+	@Override
+	public int getLZIndex() {
+		return parameter.zIndex;
+	}
+}
---
+++ src/main/java/us/dontcareabout/gxt/client/draw/LImageSprite.java
@@ -0,0 +1,88 @@
+package us.dontcareabout.gxt.client.draw;
+
+import com.sencha.gxt.chart.client.draw.sprite.ImageSprite;
+
+public class LImageSprite extends ImageSprite implements LSprite {
+	private Parameter parameter = new Parameter();
+	private Layer layer;
+
+	@Override
+	public void setLayer(Layer layer) {
+		this.layer = layer;
+	}
+
+	@Override
+	public void setX(double value) {
+		if (parameter != null && parameter.lock) {
+			throw new UnsupportedOperationException("Use setLX() instead.");
+		}
+
+		super.setX(value);
+	}
+
+	@Override
+	public void setLX(double value) {
+		parameter.x = value;
+
+		if (layer == null) { return; }
+
+		parameter.lock = false;
+		setX(layer.getX() + parameter.x);
+		parameter.lock = true;
+	}
+
+	@Override
+	public double getLX() {
+		return parameter.x;
+	}
+
+	@Override
+	public void setY(double value) {
+		if (parameter != null && parameter.lock) {
+			throw new UnsupportedOperationException("Use setLY() instead.");
+		}
+
+		super.setY(value);
+	}
+
+	@Override
+	public void setLY(double value) {
+		parameter.y = value;
+
+		if (layer == null) { return; }
+
+		parameter.lock = false;
+		setY(layer.getY() + parameter.y);
+		parameter.lock = true;
+	}
+
+	@Override
+	public double getLY() {
+		return parameter.y;
+	}
+
+	@Override
+	public void setZIndex(int zIndex) {
+		if (parameter != null && parameter.lock) {
+			throw new UnsupportedOperationException("Use setLZIndex() instead.");
+		}
+
+		super.setZIndex(zIndex);
+	}
+
+	@Override
+	public void setLZIndex(int value) {
+		parameter.zIndex = value;
+
+		if (layer == null) { return; }
+
+		parameter.lock = false;
+		setZIndex(layer.getZIndex() + parameter.zIndex);
+		parameter.lock = true;
+	}
+
+	@Override
+	public int getLZIndex() {
+		return parameter.zIndex;
+	}
+}
