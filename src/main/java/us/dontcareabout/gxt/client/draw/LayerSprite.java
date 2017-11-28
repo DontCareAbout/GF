befebde2bfe0ad1d05dbcc5f6d8fe55f79af5410
MontyPan <monty.pan@dontcareabout.us>
2017-11-28 17:04:16 +0800

    透過內建的 background sprite 使 LayerSprite 具有實際大小的特性

--- src/main/java/us/dontcareabout/gxt/client/draw/LayerSprite.java
+++ src/main/java/us/dontcareabout/gxt/client/draw/LayerSprite.java
@@ -1,12 +1,85 @@
 package us.dontcareabout.gxt.client.draw;
 
+import com.sencha.gxt.chart.client.draw.Color;
+import com.sencha.gxt.chart.client.draw.DrawComponent;
+
 /**
  * 特化版的 {@link Layer}。
  * 使其可以如 {@link LTextSprite} 等 sprite 一樣加在 {@link Layer} 上。
+ * <p>
+ * 有別於單純作為 sprite 集合的 {@link Layer}，
+ * LayerSprite 內建一個 {@link LRectangleSprite} 作為 background，
+ * 因此有實際的大小與可視範圍（但是並沒有防止 member sprite 超出範圍 XD）。
+ * caller 可在 {@link DrawComponent} onResize() 時
+ * 呼叫 {@link #onResize(double, double)} 來調整 LayerSprite 的大小，
+ * 並 override {@link #adjustMember()} 以實作各 member sprite 的對應調整。
+ * <p>
+ * 如果要調整 background 的樣式，請使用 setBg*() 系列 method，
+ * 例如 {@link #setBgRadius(double)}。
  */
 public class LayerSprite extends Layer implements LSprite {
 	private Parameter parameter = new Parameter();
 	private Layer layer;
+	private LRectangleSprite bg = new LRectangleSprite();
+
+	public LayerSprite() {
+		this(0, 0);
+	}
+
+	public LayerSprite(double x, double y) {
+		super(x, y);
+		add(bg);
+
+		bg.setFill(Color.NONE);
+	}
+
+	public final void onResize(double width, double height) {
+		if (width < 0 || height < 0) { return; }
+
+		setWidth(width);
+		setHeight(height);
+		adjustMember();
+	}
+
+	public void setWidth(double width) {
+		bg.setWidth(width);
+	}
+
+	public double getWidth() {
+		return bg.getWidth();
+	}
+
+	public void setHeight(double height) {
+		bg.setHeight(height);
+	}
+
+	public double getHeight() {
+		return bg.getHeight();
+	}
+
+	public void setBgColor(Color color) {
+		bg.setFill(color);
+	}
+
+	public Color getBgColor() {
+		return bg.getFill();
+	}
+
+	public void setBgOpacity(double opacity) {
+		bg.setOpacity(opacity);
+	}
+
+	public double getBgOpacity() {
+		return bg.getOpacity();
+	}
+
+	public void setBgRadius(double radius) {
+		bg.setRadius(radius);
+	}
+
+	public double getBgRadius() {
+		return bg.getRadius();
+	}
 
 	@Override
 	public void setLayer(Layer layer) {
@@ -79,4 +152,9 @@ public class LayerSprite extends Layer implements LSprite {
 
 		super.setZIndex(value);
 	}
+
+	/**
+	 * 在 {@link #onResize(double, double)} 時提供 child class 調整 member sprite 的時機點。
+	 */
+	protected void adjustMember() {}
 }
