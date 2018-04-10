705fd0a8e8cc79b5eefb8a215137b4b0a60cd07b
MontyPan <monty.pan@dontcareabout.us>
2018-04-10 23:52:26 +0800

    增加：SimpleContainer 版的 LayerContainer

---
+++ src/main/java/us/dontcareabout/gxt/client/draw/container/SimpleLayerContainer.java
@@ -0,0 +1,29 @@
+package us.dontcareabout.gxt.client.draw.container;
+
+import us.dontcareabout.gxt.client.draw.Layer;
+import us.dontcareabout.gxt.client.draw.LayerContainer;
+import us.dontcareabout.gxt.client.draw.LayerSprite;
+
+/**
+ * 只能加入一個 {@link LayerSprite}，
+ * 並且會讓該 {@link LayerSprite} 的大小與自身大小一樣。
+ */
+public class SimpleLayerContainer extends LayerContainer {
+	private final LayerSprite layer;
+
+	public SimpleLayerContainer(LayerSprite layer) {
+		this.layer = layer;
+		super.addLayer(layer);
+	}
+
+	@Override
+	public void addLayer(Layer layer) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	protected void onResize(int width, int height) {
+		layer.resize(width, height);
+		super.onResize(width, height);
+	}
+}
