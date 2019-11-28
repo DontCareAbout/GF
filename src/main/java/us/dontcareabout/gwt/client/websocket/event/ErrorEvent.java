51c97c624fc23663f642f216b2d3d8f406db4d17
MontyPan <monty.pan@dontcareabout.us>
2019-11-28 17:04:24 +0800

    擴大 ErrorEvent 的原始來源。SocketError 改為可存取任何名稱的 field

--- src/main/java/us/dontcareabout/gwt/client/websocket/event/ErrorEvent.java
+++ src/main/java/us/dontcareabout/gwt/client/websocket/event/ErrorEvent.java
@@ -1,16 +1,31 @@
 package us.dontcareabout.gwt.client.websocket.event;
 
-import us.dontcareabout.gwt.client.websocket.SocketError;
-
+import com.google.gwt.core.client.JavaScriptException;
 import com.google.gwt.event.shared.GwtEvent;
+
+import us.dontcareabout.gwt.client.websocket.SocketError;
 
 public class ErrorEvent extends GwtEvent<ErrorHandler>{
 	public static final GwtEvent.Type<ErrorHandler> TYPE = new GwtEvent.Type<ErrorHandler>();
 
-	private SocketError err;
+	/**
+	 * 若為 true，表示此 instance 是由 {@link JavaScriptException} 而引發，
+	 * 此時 {@link #error} 會是 null。以此類推 false。
+	 */
+	public final boolean byException;
+	public final SocketError error;
+	public final JavaScriptException exception;
 
 	public ErrorEvent(SocketError err){
-		this.err = err;
+		byException = false;
+		this.error = err;
+		this.exception = null;
+	}
+
+	public ErrorEvent(JavaScriptException jse) {
+		byException = true;
+		this.error = null;
+		this.exception = jse;
 	}
 
 	@Override
@@ -21,9 +36,5 @@ public class ErrorEvent extends GwtEvent<ErrorHandler>{
 	@Override
 	protected void dispatch(ErrorHandler handler) {
 		handler.onError(this);
-	}
-
-	public String getData(){
-		return err.getData();
 	}
 }
