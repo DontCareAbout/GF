dd2f35fc9d34a0e0de5c76322228bedb6290e9b2
MontyPan <monty.pan@dontcareabout.us>
2019-12-21 11:45:44 +0800

    增加：Validatable

---
+++ src/main/java/us/dontcareabout/gwt/client/google/Validatable.java
@@ -0,0 +1,14 @@
+package us.dontcareabout.gwt.client.google;
+
+import java.util.List;
+
+/**
+ * {@link Sheet} 會檢查對應的 {@link SheetEntry} 是否有 implement {@link Validatable}。
+ * 若有，則會執行 {@link #validate()}，通過的才會出現在 {@link Sheet#getEntry()} 當中。
+ */
+public interface Validatable {
+	/**
+	 * @return 對該 instance 作 validate，若通過 validate，則回傳 null 或是空的 {@link List}。
+	 */
+	List<Throwable> validate();
+}
