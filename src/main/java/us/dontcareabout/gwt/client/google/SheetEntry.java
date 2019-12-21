0c747a4aadce2a0be48ab140948f167e9f1d66cb
MontyPan <monty.pan@dontcareabout.us>
2019-12-21 12:49:36 +0800

    增加 validate 功能以及相關 method

--- src/main/java/us/dontcareabout/gwt/client/google/SheetEntry.java
+++ src/main/java/us/dontcareabout/gwt/client/google/SheetEntry.java
@@ -7,6 +7,8 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 /**
  * <b>注意：</b>試算表的 column 名稱不能為「GF_INDEX」。
+ *
+ * @see Validatable
  */
 public class SheetEntry extends JavaScriptObject {
 	protected SheetEntry() {}
