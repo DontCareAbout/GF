5976ad31ec041de73392e51c2133c3933cf527a3
MontyPan <monty.pan@dontcareabout.us>
2021-08-21 00:15:41 +0800

    增加：fluent API 以及補強 JavaDoc

--- src/main/java/us/dontcareabout/gwt/client/google/sheet/SheetDto.java
+++ src/main/java/us/dontcareabout/gwt/client/google/sheet/SheetDto.java
@@ -7,15 +7,64 @@ import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
 
 /**
- * 讀取 sheet 並將每一行轉成特定 object（{@link Row}）。
+ * 讀取 sheet 並將每一列轉成特定 object（{@link Row}）。
+ * 除了提供 {@link #get(String, String, String, Validator, Callback)} 外，
+ * 也可透過 fluent API 設定參數然後呼叫 {@link #fetch(Callback)}。
+ * <p>
+ * 若有指定 {@link Validator}，則只有通過 validate 的資料才會出現在 {@link Sheet#getRows()} 中。
+ * <p>
+ * 注意：取回資料的第一列的值會視為 column 值，對應到 {@link Row} 的 *Field() 的傳入值。
  */
-public class SheetDto {
-	public static String url(String sheetId, String range, String key) {
-		return "https://sheets.googleapis.com/v4/spreadsheets/"+ sheetId + "/values/" + range + "?key=" + key;
+public class SheetDto<T extends Row> {
+	private String sheetId;
+	private String key;
+	private String tabName;
+	private Validator<T> validator;
+
+	public String getSheetId() {
+		return sheetId;
 	}
 
-	public static <T extends Row> void get(String key, String sheetId, String range, Callback<T> callback) {
-		get(key, sheetId, range, null, callback);
+	public SheetDto<T> sheetId(String sheetId) {
+		this.sheetId = sheetId;
+		return this;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public SheetDto<T> key(String key) {
+		this.key = key;
+		return this;
+	}
+
+	public String getTabName() {
+		return tabName;
+	}
+
+	public SheetDto<T> tabName(String tabName) {
+		this.tabName = tabName;
+		return this;
+	}
+
+	public Validator<T> getValidator() {
+		return validator;
+	}
+
+	public SheetDto<T> validator(Validator<T> validator) {
+		this.validator = validator;
+		return this;
+	}
+
+	public void fetch(Callback<T> callback) {
+		get(key, sheetId, tabName, validator, callback);
+	}
+
+	////////////////
+
+	public static String url(String sheetId, String range, String key) {
+		return "https://sheets.googleapis.com/v4/spreadsheets/"+ sheetId + "/values/" + range + "?key=" + key;
 	}
 
 	public static <T extends Row> void get(String key, String sheetId, String range, Validator<T> validator, Callback<T> callback) {
